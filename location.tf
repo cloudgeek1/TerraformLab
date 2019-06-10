e0428622a4abd3d48f2b3230bdf0f8599ca4d890
cloudgeek1 <cloudgeek1@gmail.com>
2019-06-10 17:38:11 -0500

    Update and rename locationA to location
    Update and rename

--- location.tf
+++ location.tf
@@ -1,16 +1,15 @@
-variable "web_server_location_eu" {}
+variable "web_server_location" {}
 variable "web_server_rg" {}
 variable "resource_prefix" {}
-variable "web_server_address_space_eu" {}
+variable "web_server_address_space" {}
 variable "web_server_name" {}
 variable "environment" {}
 variable "web_server_count" {}
-variable "web_server_subnets_eu" {
+variable "web_server_subnets" {
   type = "list"
 }
 variable "terraform_script_version" {}
 variable "domain_name_label" {}
-
 
 locals {
   web_server_name   = "${var.environment == "production" ? "${var.web_server_name}-prd" : "${var.web_server_name}-dev"}"
@@ -19,7 +18,7 @@ locals {
 
 resource "azurerm_resource_group" "web_server_rg" {
   name     = "${var.web_server_rg}"
-  location = "${var.web_server_location_eu}"
+  location = "${var.web_server_location}"
 
 tags = {
   environment   = "${local.build_environment}"
@@ -29,23 +28,23 @@ tags = {
 
 resource "azurerm_virtual_network" "web_server_vnet" {
   name                = "${var.resource_prefix}-vnet"
-  location            = "${var.web_server_location_eu}"
+  location            = "${var.web_server_location}"
   resource_group_name = "${azurerm_resource_group.web_server_rg.name}"
-  address_space       = ["${var.web_server_address_space_eu}"]
+  address_space       = ["${var.web_server_address_space}"]
 }
 
 resource "azurerm_subnet" "web_server_subnet" {
-  name                      = "${var.resource_prefix}-${substr(var.web_server_subnets_eu[count.index], 0, length(var.web_server_subnets_eu[count.index]) - 3)}-subnet"
+  name                      = "${var.resource_prefix}-${substr(var.web_server_subnets[count.index], 0, length(var.web_server_subnets[count.index]) - 3)}-subnet"
   resource_group_name       = "${azurerm_resource_group.web_server_rg.name}"
   virtual_network_name      = "${azurerm_virtual_network.web_server_vnet.name}"
-  address_prefix            = "${var.web_server_subnets_eu[count.index]}"
+  address_prefix            = "${var.web_server_subnets[count.index]}"
   network_security_group_id = "${count.index == 0 ? "${azurerm_network_security_group.web_server_nsg.id}" : ""}"
-  count                     = "${length(var.web_server_subnets_eu)}"
+  count                     = "${length(var.web_server_subnets)}"
 }
 
 resource "azurerm_public_ip" "web_server_lb_public_ip" {
   name                         = "${var.resource_prefix}-public-ip"
-  location                     = "${var.web_server_location_eu}"
+  location                     = "${var.web_server_location}"
   resource_group_name          = "${azurerm_resource_group.web_server_rg.name}"
   public_ip_address_allocation = "${var.environment == "production" ? "static" : "dynamic"}"
   domain_name_label            = "${var.domain_name_label}"
@@ -53,7 +52,7 @@ resource "azurerm_public_ip" "web_server_lb_public_ip" {
 
 resource "azurerm_network_security_group" "web_server_nsg" {
   name                = "${var.resource_prefix}-nsg"
-  location            = "${var.web_server_location_eu}"
+  location            = "${var.web_server_location}"
   resource_group_name = "${azurerm_resource_group.web_server_rg.name}" 
 }
 
@@ -71,9 +70,9 @@ resource "azurerm_network_security_rule" "web_server_nsg_rule_http" {
   network_security_group_name = "${azurerm_network_security_group.web_server_nsg.name}" 
 }
 
-resource "azurerm_virtual_machine_scale_set" "web_server_eu" {
+resource "azurerm_virtual_machine_scale_set" "web_server" {
   name                         = "${var.resource_prefix}-scale-set"
-  location                     = "${var.web_server_location_eu}"
+  location                     = "${var.web_server_location}"
   resource_group_name          = "${azurerm_resource_group.web_server_rg.name}"  
   upgrade_policy_mode          = "manual"
 
@@ -96,7 +95,7 @@ resource "azurerm_virtual_machine_scale_set" "web_server_eu" {
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
-
+  
   os_profile {
     computer_name_prefix = "${local.web_server_name}" 
     admin_username       = "webserver"
@@ -112,39 +111,39 @@ resource "azurerm_virtual_machine_scale_set" "web_server_eu" {
     primary = true
 
     ip_configuration {
-      name                                   = "${local.web_server_name}"
+      name                                   = "${local.web_server_name}" 
       primary                                = true
-      subnet_id                              = "${azurerm_subnet.web_server_subnets_eu.*.id[0]}"
+      subnet_id                              = "${azurerm_subnet.web_server_subnet.*.id[0]}"
       load_balancer_backend_address_pool_ids = ["${azurerm_lb_backend_address_pool.web_server_lb_backend_pool.id}"]
     }
   }
 
+  extension {
+    name                 = "${local.web_server_name}-extension" 
+    publisher            = "Microsoft.Compute"
+    type                 = "CustomScriptExtension"
+    type_handler_version = "1.9"
 
-
-extension {
-  name                 = "${local.web_server_name}-extension"
-  publisher            = "Microsoft.Compute"
-  type                 = "CustomScriptExtension"
-  type_handler_version = "1.9"
-
-  settings = <<SETTINGS
+    settings = <<SETTINGS
     {
       "fileUris": ["https://raw.githubusercontent.com/eltimmo/learning/master/azureInstallWebServer.ps1"],
       "commandToExecute": "start powershell -ExecutionPolicy Unrestricted -File azureInstallWebServer.ps1"
     }
     SETTINGS
-}
+
+  }
+
 }
 
 resource "azurerm_lb" "web_server_lb" {
   name                = "${var.resource_prefix}-lb"
-  location            = "${var.web_server_location_eu}"
+  location            = "${var.web_server_location}"
   resource_group_name = "${azurerm_resource_group.web_server_rg.name}" 
 
   frontend_ip_configuration {
      name                 = "${var.resource_prefix}-lb-frontend-ip"
      public_ip_address_id = "${azurerm_public_ip.web_server_lb_public_ip.id}"
-  }
+  }  
 }
 
 resource "azurerm_lb_backend_address_pool" "web_server_lb_backend_pool" {
@@ -172,4 +171,3 @@ resource "azurerm_lb_rule" "web_server_lb_http_rule" {
   probe_id                       = "${azurerm_lb_probe.web_server_lb_http_probe.id}"
   backend_address_pool_id        = "${azurerm_lb_backend_address_pool.web_server_lb_backend_pool.id}"
 }
-
